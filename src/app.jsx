7bcdb379ed6e0f77182a5196eb72794311bb953b
Julie <ssabini.sl@gmail.com>
2021-10-11 16:16:50 +0900

    make video item

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,26 @@
+import { useEffect, useState } from 'react';
 import './app.css';
+import VideoList from './components/video_list/video_list';
 
 function App() {
-  return <h1>Hello :) </h1>;
+  const [videos, setVideos] = useState([]); // 변수와 업데이트할 수 있는 함수가 각각 할당됨. 초기값은 비워진 목록
+
+  //마운트가 되었거나 콜백이 되었을때 사용하는 api. 원하는 함수를 등록해놓으면 마운트될때, 콜백되었을때 호출되게 됨
+  // 두번째 인자로 아무것도 전달하지않으면 컴포넌트에 state나 prop이 업데이트가 될때마다 무조건 반복 호출
+  // [] 텅텅 빈 배열을 두번째 인자로 전달하면 마운트 되었을때만 호출
+  // [names] 라고 입력하면 names이 바뀌었을때만 호출함 
+  useEffect(()=>{
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch("https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyCo6dwKiTc_1_R-y6QwJSUu2of3E03oXQs", requestOptions)
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
+      .catch(error => console.log('error', error));
+  }, []); 
+  return <VideoList videos={videos}/>;
 }
 
 export default App;
